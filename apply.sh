641d58ba72321da516296dfeac21b057d461e87e
Madhav Khosla <madhav.khoslaa@gmail.com>
2023-12-17 11:54:14 +0530

    Initial Commit

---
+++ apply.sh
@@ -0,0 +1,4 @@
+mkdir ~/.config
+mkdir ~/.fonts
+cp -rT config ~/.config
+cp -rT fonts ~/.fonts
---
+++ backup.sh
@@ -0,0 +1,6 @@
+mkdir ~/Backup
+mkdir ~/Backup/config
+mkdir ~/Backup/fonts
+cp -rT ~/.config/ ~/Backup/config
+cp -rT ~/.fonts ~/Backup/fonts
+
